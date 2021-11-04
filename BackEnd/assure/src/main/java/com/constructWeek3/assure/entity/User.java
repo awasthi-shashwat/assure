c79d8a1bdb3243d767b527314433ff26309c5bfb
Mukul Howale <howalemukul@gmail.com>
2021-11-05 02:08:36 +0530

    added exception folder, added modelmapper seperate folder

--- BackEnd/assure/src/main/java/com/constructWeek3/assure/entity/User.java
+++ BackEnd/assure/src/main/java/com/constructWeek3/assure/entity/User.java
@@ -1,13 +1,25 @@
 package com.constructWeek3.assure.entity;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class User {
 
     @Id
     @GeneratedValue
     private Long userId;
+
+    private String userName;
+    private String userEmail;
+    private String userMobile;
+    private String userPass;
 }
