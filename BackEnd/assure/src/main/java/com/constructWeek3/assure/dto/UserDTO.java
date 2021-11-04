f09ee667df376a1b4102f3b350e12dde384da5a7
Mukul Howale <howalemukul@gmail.com>
2021-11-04 23:17:49 +0530

    added user files and dto folder

---
+++ BackEnd/assure/src/main/java/com/constructWeek3/assure/dto/UserDTO.java
@@ -0,0 +1,5 @@
+package com.constructWeek3.assure.dto;
+
+
+public class UserDTO {
+}
---
+++ BackEnd/assure/src/main/java/com/constructWeek3/assure/entity/User.java
@@ -0,0 +1,13 @@
+package com.constructWeek3.assure.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long userId;
+}
