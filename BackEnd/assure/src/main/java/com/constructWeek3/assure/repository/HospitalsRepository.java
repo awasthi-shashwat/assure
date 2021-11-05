6f1daf0b45f88507ad77c50650e8ffadbbf11124
Mukul Howale <howalemukul@gmail.com>
2021-11-06 02:16:00 +0530

    update

--- BackEnd/assure/src/main/java/com/constructWeek3/assure/repository/HospitalsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.constructWeek3.assure.repository;
-
-import com.constructWeek3.assure.entity.Hospitals;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HospitalsRepository extends JpaRepository<Hospitals, Long> {
-}
