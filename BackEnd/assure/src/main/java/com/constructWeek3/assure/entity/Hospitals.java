e21b4ff3ff821b808cd6d87ecb6f5bc0c3758411
Ravi Mane <meravimane@gmail.com>
2021-11-09 23:44:26 +0530

    Adding relationship

--- BackEnd/assure/src/main/java/com/constructWeek3/assure/entity/Hospitals.java
+++ BackEnd/assure/src/main/java/com/constructWeek3/assure/entity/Hospitals.java
@@ -1,8 +1,14 @@
 package com.constructWeek3.assure.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 public class Hospitals {
 
     @Id
@@ -10,6 +16,7 @@ public class Hospitals {
     private Long hospitalId;
     private String name;
 
+    @JsonIgnore
     @ManyToOne
     private Location location;
 }
--- BackEnd/assure/src/main/java/com/constructWeek3/assure/entity/Location.java
+++ BackEnd/assure/src/main/java/com/constructWeek3/assure/entity/Location.java
@@ -1,13 +1,15 @@
 package com.constructWeek3.assure.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -19,4 +21,28 @@ public class Location {
     @Id
     Long locationId;
     String name;
+
+
+
+
+    @ManyToMany
+    @JoinTable(name = "Location_Policies",
+            joinColumns = @JoinColumn(name = "Location_Id"),
+            inverseJoinColumns = @JoinColumn(name = "Policy_Id"))
+    public List<Policy> policies = new ArrayList<>();
+
+
+    @OneToMany(mappedBy = "location")
+    private List<Hospitals> hospitalsList = new ArrayList<>();
+
+
+    public void addHospital(Hospitals hospital){
+        this.hospitalsList.add(hospital);
+    }
+
+    public void addPolicies(Policy policy){
+    this.policies.add(policy);
+    }
+
+
 }
