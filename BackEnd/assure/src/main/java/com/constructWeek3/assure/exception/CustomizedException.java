48b1bbd1f582909207d309f1299545e372403ac1
Prottay Ray <prottayray@gmail.com>
2021-11-10 14:35:22 +0530

    little fix for exception

--- BackEnd/assure/src/main/java/com/constructWeek3/assure/exception/CustomizedException.java
+++ BackEnd/assure/src/main/java/com/constructWeek3/assure/exception/CustomizedException.java
@@ -3,12 +3,14 @@ package com.constructWeek3.assure.exception;
 import com.constructWeek3.assure.dto.ExceptionDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 
+@ControllerAdvice
 public class CustomizedException extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(InvalidAgeOfMemberException.class)
