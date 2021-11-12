793120d0d98055d7b5e35cb55f088ac5935c75c1
Prottay Ray <prottayray@gmail.com>
2021-11-12 19:29:32 +0530

    uploaded the payment section static

---
+++ FrontEnd/scripts/payment.js
@@ -0,0 +1,3 @@
+let visibleSection = document.getElementById("visibleRegion");
+let processingSection = document.getElementById("processing");
+let doneSection = document.getElementById("payment_success_section");
