7e762e3d089d1757a0927c136139cc35f9f5198e
Ravi Mane <meravimane@gmail.com>
2021-11-13 14:00:33 +0530

    Adding JavaScript for Direct claim

---
+++ FrontEnd/scripts/directClaim2.js
@@ -0,0 +1,18 @@
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    
+    // console.log(JSON.stringify(Object.fromEntries(formData)));
+    // console.log(Object.fromEntries(formData));
+    addToTreatmentDocumentDetails(Object.fromEntries(formData));
+});
+if(localStorage.getItem("treatmentDocuments")==null){
+    localStorage.setItem("treatmentDocuments", JSON.stringify([]));
+}
+
+function addToTreatmentDocumentDetails(p){
+    let openTreatmentDocumentDetails = JSON.parse(localStorage.getItem("treatmentDocuments"));
+    openTreatmentDocumentDetails.push(p);
+    localStorage.setItem("treatmentDOcuments", JSON.stringify(openTreatmentDocumentDetails));
+}
