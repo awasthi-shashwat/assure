b8eaf597f338a840023efd738a50442044f05102
Abhinav <abhinavgujral33@gmail.com>
2021-11-13 11:04:36 +0530

    linking claim page fixes

--- FrontEnd/scripts/claimStep2.js
+++ FrontEnd/scripts/claimStep2.js
@@ -1,6 +1,8 @@
 let selected_claim=JSON.parse(localStorage.getItem("choosen_claim"));
 document.getElementById("claim_heading").innerText=selected_claim.selected;
-
+if(selected_claim.selected=="Call & Claim"){
+    document.getElementById("call_icon").classList.remove('d-none');
+}
 function submit(){
     console.log("submit");
     document.getElementById("form_claim").classList.remove("d-block");
