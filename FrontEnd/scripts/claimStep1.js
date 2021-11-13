cbc8c312d17276c2f2c9f34bee190a6db4f22abc
Abhinav <abhinavgujral33@gmail.com>
2021-11-13 16:04:43 +0530

    explore 2nd page fixes

--- FrontEnd/scripts/claimStep1.js
+++ FrontEnd/scripts/claimStep1.js
@@ -3,4 +3,18 @@ document.getElementById("claim_heading").innerText=selected_claim.selected;
 
 if(selected_claim.selected=="Call & Claim"){
     document.getElementById("call_icon").classList.remove('d-none');
+
+
+    
+}
+
+function storedetails(){
+    let claim_details=
+    { 
+    Location :document.getElementById("select_location").value,
+    hospitalName:document.getElementById("select_hospital").value,
+    PolicyNumber:document.getElementById('select_policy').value
+    };
+    localStorage.setItem('claim_details',JSON.stringify(claim_details));
+    location.href="./claim_step2.html";
 }
